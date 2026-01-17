5bf485e62584ecb22f68023edfe2941b0b27b4a6
fabiorollin <fabiorollin@gmail.com>
2026-01-17 16:36:03 -0500

    Add Address and Product Class

---
+++ Lab 1/src/Model/Address.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author fabio
+ */
+class Address {
+    String StreetName;
+    String unitNum;
+    String city;
+    String zipCode;
+
+    public String getStreetName() {
+        return StreetName;
+    }
+
+    public void setStreetName(String StreetName) {
+        this.StreetName = StreetName;
+    }
+
+    public String getUnitNum() {
+        return unitNum;
+    }
+
+    public void setUnitNum(String unitNum) {
+        this.unitNum = unitNum;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    
+    
+}
---
+++ Lab 1/src/Model/Product.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author fabio
+ */
+public class Product {
+    String name;
+    String description;
+    String availNum;
+    String price;
+    Address manufactureAddress;
+    Address shippingAdrress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Address getManufactureAddress() {
+        return manufactureAddress;
+    }
+
+    public void setManufactureAddress(Address manufactureAddress) {
+        this.manufactureAddress = manufactureAddress;
+    }
+
+    public Address getShippingAdrress() {
+        return shippingAdrress;
+    }
+
+    public void setShippingAdrress(Address shippingAdrress) {
+        this.shippingAdrress = shippingAdrress;
+    }
+    
+    
+}
